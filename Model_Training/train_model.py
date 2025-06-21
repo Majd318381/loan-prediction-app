859b7b171e911c5555c196dcaa40b37f6fd38e31
mohammed <mohammed@gmail.com>
2025-06-21 18:37:00 +0300

    modeifed code for add encoders model

--- Model_Training/train_model.py
+++ Model_Training/train_model.py
@@ -17,17 +17,28 @@ import plotly.express as px
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import joblib
 import pickle
+
 ### Read dataset
+
 data = pd.read_csv('loan_prediction.csv') #import the train dataset
 
+
 data.head()
+
 data.info() 
+
 ### print out sum of null values in each column
+
 data.isnull().sum() 
+
 ### I used the seaborn heatmap to visualize null values in each column
+
 sns.heatmap(data.isnull(),yticklabels=False,cbar=False,cmap='viridis') 
 
+
+
 # Relationship between Gender and Loan Status:
+
 
 
 Gender = ['Male', 'Female']
@@ -45,7 +56,9 @@ plt.ylabel('Count', fontsize=16)
 plt.title('Gender vs Loan status',fontsize=18)
 plt.legend(Loan_Status,loc=1)
 plt.show()
+
 # Relationship between marital status vs loan status
+
 
 
 MaritalStatus=['Yes', 'No']
@@ -63,7 +76,9 @@ plt.ylabel('Count', fontsize=16)
 plt.title('Marital Status vs Loan status',fontsize=18)
 plt.legend(Loan_Status,loc=1)
 plt.show()
+
 # Relationship Between Dependents vs Loan status
+
 
 
 
@@ -82,7 +97,9 @@ plt.ylabel('Count', fontsize=16)
 plt.title('Dependents vs Loan status',fontsize=18)
 plt.legend(Loan_Status,loc=1)
 plt.show()
+
 # Relationship between education vs Loan status:
+
 
 
 
@@ -101,7 +118,9 @@ plt.ylabel('Count', fontsize=16)
 plt.title('Education vs Loan status',fontsize=18)
 plt.legend(Loan_Status,loc=1)
 plt.show()
+
 # Relationship between Self-Employed vs Loan_Status:
+
 Self_Employed=['Yes', 'No']
 Loan_Status=['Yes', 'No']
 pos=np.arange(len(Self_Employed))
@@ -117,7 +136,9 @@ plt.ylabel('Count', fontsize=16)
 plt.title('Self Employed vs Loan status',fontsize=18)
 plt.legend(Loan_Status,loc=1)
 plt.show()
+
 # Relationship between Property area and loan status
+
 
 
 Property_Area=['Rural','Semiurban','Urban']
@@ -136,7 +157,9 @@ plt.title('Property Area vs Loan Status',fontsize=18)
 plt.legend(Loan_Status,loc=1)
 plt.show()
 
+
 #  Relationship between Credit History and Loan Status: 
+
 
 
 Credit_History=['Bad','Medium','Good']
@@ -154,17 +177,24 @@ plt.ylabel('Count', fontsize=16)
 plt.title('Credit History vs Loan Status',fontsize=18)
 plt.legend(Loan_Status,loc=2)
 plt.show()
+
 ### Plot a  correlation heatmap
+
 plt.figure(figsize=(10,8))
 sns.heatmap(data.corr(), annot=True, cmap='coolwarm');
+
 # Print out column with unique values
+
 
 for col in data.columns:
     if data[col].dtypes == 'object':
         num_of_unique_cat = len (data[col].unique())
         print("Features '{col_name}' has '{unique_cat}' unique categories". format(col_name=col, unique_cat=num_of_unique_cat))
+
 # Perpocessing Data 
+
 ## Replace The Null Value
+
 #Now let’s fill in the missing values. In categorical columns, we can fill in missing values with the mode of each column.
 #The mode represents the value that appears most often in the column and is an appropriate choice when dealing with categorical data
 data['Gender'].fillna(data['Gender'].mode()[0], inplace=True)
@@ -183,26 +213,34 @@ data['Loan_Amount_Term'].fillna(data['Loan_Amount_Term'].mode()[0], inplace=True
 #Since credit history is a binary variable (0 or 1), the mode represents the most common value and is an appropriate choice for filling in missing values.
 data['Credit_History'].fillna(data['Credit_History'].mode()[0], inplace=True)
 
+
 ### plot heatmap after replacing null value
+
 sns.heatmap(data.isnull(),yticklabels=False,cbar=False,cmap='viridis') #i used the seaborn heatmap to visualize null values in each column
+
 ### Scatter plot 
+
 
 plt.figure(figsize=(10, 6))
 sns.scatterplot(data=data, x='ApplicantIncome', y='LoanAmount')
 plt.xlabel('Applicant Income')
 plt.ylabel('Loan Amount')
 plt.title('Scatter Plot: Applicant Income vs. Loan Amount')
-plt.savefig('Scatter Plot.png')
+
 plt.show()
 
+
 ### *Detecting outliers and removing them*ted outliers
+
 plt.figure(figsize=(10, 6))
 sns.boxplot(data=data, x='Loan_Status', y='ApplicantIncome')
 plt.ylabel('Applicant Income')
 plt.xlabel('Education')
 plt.title('Box Plot: Applicant Income vs. Education')
-plt.savefig('outliers.png')
+
 plt.show()
+
+
 
 # The “ApplicantIncome” column contains outliers which need to be removed before moving further. Here’s how to remove the outliers:
 
@@ -245,16 +283,20 @@ data= data[(data['CoapplicantIncome'] >= lower_bound) & (data['CoapplicantIncome
 
 
 
+
+
 plt.figure(figsize=(10, 6))
 sns.boxplot(data=data, x='Loan_Status', y='ApplicantIncome')
 plt.ylabel('Applicant Income')
 plt.xlabel('Education')
 plt.title('Box Plot: Applicant Income vs. Education')
-plt.savefig('Scatter Plot.png')
+
 plt.show()
 
 
+
 ### The scatter plot after remove outliers from ApplicantIncome
+
 
 
 plt.figure(figsize=(10, 6))
@@ -262,8 +304,9 @@ sns.scatterplot(data=data, x='ApplicantIncome', y='LoanAmount')
 plt.xlabel('Applicant Income')
 plt.ylabel('Loan Amount')
 plt.title('Scatter Plot: Applicant Income vs. Loan Amount')
-plt.savefig('scatterafter.png')
+
 plt.show()
+
 
 
 
@@ -275,32 +318,42 @@ plt.show()
 
 
 
+
 # Scale the numerical columns using StandardScaler
 scaler = StandardScaler()
 numerical_cols = ['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term']
 data[numerical_cols] = scaler.fit_transform(data[numerical_cols])
 data[numerical_cols] = scaler.transform(data[numerical_cols])
 
+
 ### Convert categorical Columns into numerical ones
 
+encoders = {}
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
 col_to_encode = ['Gender','Dependents', 'Married', 'Education', 'Self_Employed', 'Property_Area','Loan_Status']
 for i in col_to_encode:
     data[i] = le.fit_transform(data[i])
+    encoders[i] = le
+# Save encoders for future use
+joblib.dump(encoders, 'encoders.pkl')
 
 data.head()
+
 data = data.drop(['Loan_ID'], axis = 1)
+
 # Split the dataset into features (X) and target (y)
 
 X = data.drop('Loan_Status', axis=1)
 y = data['Loan_Status']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,random_state=42)
+
 model = LogisticRegression() #define the model
 model.fit(X_train, y_train) #fit the model
 ypred = model.predict(X_test) #predict on test sample
 evaluation = f1_score(y_test, ypred)
 evaluation
+
 
 import pandas as pd
 import numpy as np
@@ -317,7 +370,9 @@ from sklearn.svm import SVC
 import warnings
 warnings.filterwarnings("ignore")
 
+
 ### Train the model
+
 log_model = LogisticRegression()
 log_model.fit(X_train, y_train)
 ypred_log = log_model.predict(X_test)
@@ -327,4 +382,10 @@ print("Accuracy:", accuracy_score(y_test, ypred_log))
 print("Precision:", precision_score(y_test, ypred_log))
 print("Recall:", recall_score(y_test, ypred_log))
 print("F1 Score:", f1_score(y_test, ypred_log))
-joblib.dump(model, 'finall_loan_model.pkl')
+
+joblib.dump(log_model, 'finall_loan_model.pkl')
+
+
+
+
+
