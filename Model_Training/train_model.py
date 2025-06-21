08e57cc04fb521738d7d72d0701208f0a2fcb432
mohammed <mohammed@gmail.com>
2025-06-21 17:25:14 +0300

    modified the train_model file

--- Model_Training/train_model.py
+++ Model_Training/train_model.py
@@ -1,61 +1,330 @@
+#Import Libraries
+import os
+from sklearn.preprocessing import StandardScaler
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+sns.set()
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from numpy import loadtxt
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+import plotly.express as px
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-from sklearn.preprocessing import LabelEncoder
-from sklearn.impute import SimpleImputer
-from sklearn.ensemble import RandomForestClassifier
 import joblib
+import pickle
+### Read dataset
+data = pd.read_csv('loan_prediction.csv') #import the train dataset
 
-df = pd.read_csv('loan_prediction.csv')
+data.head()
+data.info() 
+### print out sum of null values in each column
+data.isnull().sum() 
+### I used the seaborn heatmap to visualize null values in each column
+sns.heatmap(data.isnull(),yticklabels=False,cbar=False,cmap='viridis') 
 
-# Preprocessing the dataset
-df = df.drop('Loan_ID', axis=1)
+# Relationship between Gender and Loan Status:
 
-# Fill missing values for categorical columns with the most frequent value
-imputer = SimpleImputer(strategy='most_frequent')
-df_imputed = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
 
-df.info()
+Gender = ['Male', 'Female']
+Loan_Status = ['Yes', 'No']
+pos= np.arange(len(Gender))
+bar_width= 0.30
+Loan_Status_Yes= [347,75]
+Loan_Status_NO= [155,37]
 
-# Convert categorical columns to numerical using Label Encoding
-encoders = {}
-for column in df.select_dtypes(include='object').columns.tolist():
-    print(f"Encoding column: {column}")
-    le = LabelEncoder()
-    df_imputed[column] = le.fit_transform(df_imputed[column])
-    encoders[column] = le
+plt.bar(pos,Loan_Status_Yes,bar_width,color='green',edgecolor='black')
+plt.bar(pos+bar_width,Loan_Status_NO,bar_width,color='orange',edgecolor='black')
+plt.xticks(pos, Gender)
+plt.xlabel('Gender', fontsize=16)
+plt.ylabel('Count', fontsize=16)
+plt.title('Gender vs Loan status',fontsize=18)
+plt.legend(Loan_Status,loc=1)
+plt.show()
+# Relationship between marital status vs loan status
 
-# Save encoders for future use
-joblib.dump(encoders, 'encoders.pkl')
-print("Encoders are saved as encoders.pkl")
 
-# X is the feature set, y is the target variable
-X = df_imputed.drop('Loan_Status', axis=1)
-y = df_imputed['Loan_Status']
+MaritalStatus=['Yes', 'No']
+Loan_Status=['Yes', 'No']
+pos=np.arange(len(MaritalStatus))
+bar_width=0.30
+Loan_Status_Yes=[288,134]
+Loan_Status_NO=[113,79]
 
-# Splitting the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+plt.bar(pos,Loan_Status_Yes,bar_width,color='green',edgecolor='black')
+plt.bar(pos+bar_width,Loan_Status_NO,bar_width,color='orange',edgecolor='black')
+plt.xticks(pos, MaritalStatus)
+plt.xlabel('Marital Status', fontsize=16)
+plt.ylabel('Count', fontsize=16)
+plt.title('Marital Status vs Loan status',fontsize=18)
+plt.legend(Loan_Status,loc=1)
+plt.show()
+# Relationship Between Dependents vs Loan status
 
-# Training the model
-# model = LogisticRegression(max_iter=2000)
-# model.fit(X_train, y_train)
-# Stopped using this model due to convergence issues, see the warning below
-## ConvergenceWarning: lbfgs failed to converge after 2000 iteration(s) (status=1):
-## STOP: TOTAL NO. OF ITERATIONS REACHED LIMIT
 
-model = RandomForestClassifier(random_state=42)
-model.fit(X_train, y_train)
 
-# Evaluating the model
-y_pred = model.predict(X_test)
+Dependents=['Dpdnt_No', 'Dpdnt_1', 'Dpdnt_2', 'Dpdnt_3']
+Loan_Status=['Yes', 'No']
+pos=np.arange(len(Dependents))
+bar_width=0.30
+Loan_Status_Yes=[247,66,76,33]
+Loan_Status_NO=[113,36,25,18]
 
-# Print evaluation metrics
-print("Accuracy:", accuracy_score(y_test, y_pred))
-print("Precision:", precision_score(y_test, y_pred))
-print("Recall:", recall_score(y_test, y_pred))
-print("F1 Score:", f1_score(y_test, y_pred))
+plt.bar(pos,Loan_Status_Yes,bar_width,color='green',edgecolor='black')
+plt.bar(pos+bar_width,Loan_Status_NO,bar_width,color='orange',edgecolor='black')
+plt.xticks(pos, Dependents)
+plt.xlabel('Dependents', fontsize=16)
+plt.ylabel('Count', fontsize=16)
+plt.title('Dependents vs Loan status',fontsize=18)
+plt.legend(Loan_Status,loc=1)
+plt.show()
+# Relationship between education vs Loan status:
 
-# Saving the model
-joblib.dump(model, 'loan_model.pkl')
-print("Model saved as loan_model.pkl")
+
+
+Education=['Graduate', 'Non-Graduate']
+Loan_Status=['Yes', 'No']
+pos=np.arange(len(Education))
+bar_width=0.30
+Loan_Status_Yes=[340,82]
+Loan_Status_NO=[140,52]
+
+plt.bar(pos,Loan_Status_Yes,bar_width,color='green',edgecolor='black')
+plt.bar(pos+bar_width,Loan_Status_NO,bar_width,color='orange',edgecolor='black')
+plt.xticks(pos, Education)
+plt.xlabel('Education', fontsize=16)
+plt.ylabel('Count', fontsize=16)
+plt.title('Education vs Loan status',fontsize=18)
+plt.legend(Loan_Status,loc=1)
+plt.show()
+# Relationship between Self-Employed vs Loan_Status:
+Self_Employed=['Yes', 'No']
+Loan_Status=['Yes', 'No']
+pos=np.arange(len(Self_Employed))
+bar_width=0.30
+Loan_Status_Yes=[56,366]
+Loan_Status_NO=[26,166]
+
+plt.bar(pos,Loan_Status_Yes,bar_width,color='green',edgecolor='black')
+plt.bar(pos+bar_width,Loan_Status_NO,bar_width,color='orange',edgecolor='black')
+plt.xticks(pos, Self_Employed)
+plt.xlabel('Self Employed', fontsize=16)
+plt.ylabel('Count', fontsize=16)
+plt.title('Self Employed vs Loan status',fontsize=18)
+plt.legend(Loan_Status,loc=1)
+plt.show()
+# Relationship between Property area and loan status
+
+
+Property_Area=['Rural','Semiurban','Urban']
+Loan_Status=['Yes', 'No']
+pos=np.arange(len(Property_Area))
+bar_width=0.35
+Loan_Status_Yes=[110,179,133]
+Loan_Status_NO=[69,54,69]
+
+plt.bar(pos,Loan_Status_Yes,bar_width,color='green',edgecolor='black')
+plt.bar(pos+bar_width,Loan_Status_NO,bar_width,color='orange',edgecolor='black')
+plt.xticks(pos, Property_Area)
+plt.xlabel('Property Area', fontsize=16)
+plt.ylabel('Count', fontsize=16)
+plt.title('Property Area vs Loan Status',fontsize=18)
+plt.legend(Loan_Status,loc=1)
+plt.show()
+
+#  Relationship between Credit History and Loan Status: 
+
+
+Credit_History=['Bad','Medium','Good']
+Loan_Status=['Yes', 'No']
+pos=np.arange(len(Credit_History))
+bar_width=0.35
+Loan_Status_Yes=[7,37,378]
+Loan_Status_NO=[82,13,97]
+
+plt.bar(pos,Loan_Status_Yes,bar_width,color='green',edgecolor='black')
+plt.bar(pos+bar_width,Loan_Status_NO,bar_width,color='orange',edgecolor='black')
+plt.xticks(pos, Credit_History)
+plt.xlabel('Credit History', fontsize=16)
+plt.ylabel('Count', fontsize=16)
+plt.title('Credit History vs Loan Status',fontsize=18)
+plt.legend(Loan_Status,loc=2)
+plt.show()
+### Plot a  correlation heatmap
+plt.figure(figsize=(10,8))
+sns.heatmap(data.corr(), annot=True, cmap='coolwarm');
+# Print out column with unique values
+
+for col in data.columns:
+    if data[col].dtypes == 'object':
+        num_of_unique_cat = len (data[col].unique())
+        print("Features '{col_name}' has '{unique_cat}' unique categories". format(col_name=col, unique_cat=num_of_unique_cat))
+# Perpocessing Data 
+## Replace The Null Value
+#Now let’s fill in the missing values. In categorical columns, we can fill in missing values with the mode of each column.
+#The mode represents the value that appears most often in the column and is an appropriate choice when dealing with categorical data
+data['Gender'].fillna(data['Gender'].mode()[0], inplace=True)
+data['Married'].fillna(data['Married'].mode()[0], inplace=True)
+data['Dependents'].fillna(data['Dependents'].mode()[0], inplace=True)
+data['Self_Employed'].fillna(data['Self_Employed'].mode()[0], inplace=True)
+# Fill missing values in numerical column
+#We can fill in the missing values of the loan amount column with the median value.
+#The median is an appropriate measure to fill in missing values when dealing with skewed distributions or when outliers are present in the data.
+data['LoanAmount'].fillna(data['LoanAmount'].median(), inplace=True)
+
+#We can fill in the missing values of the loan amount term column with the mode value of the column. Since the term of the loan amount is a discrete value, the mode is an appropriate metric to use
+data['Loan_Amount_Term'].fillna(data['Loan_Amount_Term'].mode()[0], inplace=True)
+
+#We can fill in the missing values of the credit history column with the mode value.
+#Since credit history is a binary variable (0 or 1), the mode represents the most common value and is an appropriate choice for filling in missing values.
+data['Credit_History'].fillna(data['Credit_History'].mode()[0], inplace=True)
+
+### plot heatmap after replacing null value
+sns.heatmap(data.isnull(),yticklabels=False,cbar=False,cmap='viridis') #i used the seaborn heatmap to visualize null values in each column
+### Scatter plot 
+
+plt.figure(figsize=(10, 6))
+sns.scatterplot(data=data, x='ApplicantIncome', y='LoanAmount')
+plt.xlabel('Applicant Income')
+plt.ylabel('Loan Amount')
+plt.title('Scatter Plot: Applicant Income vs. Loan Amount')
+plt.savefig('Scatter Plot.png')
+plt.show()
+
+### *Detecting outliers and removing them*ted outliers
+plt.figure(figsize=(10, 6))
+sns.boxplot(data=data, x='Loan_Status', y='ApplicantIncome')
+plt.ylabel('Applicant Income')
+plt.xlabel('Education')
+plt.title('Box Plot: Applicant Income vs. Education')
+plt.savefig('outliers.png')
+plt.show()
+
+# The “ApplicantIncome” column contains outliers which need to be removed before moving further. Here’s how to remove the outliers:
+
+# Calculate the IQR
+Q1 = data['ApplicantIncome'].quantile(0.25)
+Q3 = data['ApplicantIncome'].quantile(0.75)
+IQR = Q3 - Q1
+
+# Define the lower and upper bounds for outliers
+lower_bound = Q1 - 1.5 * IQR
+upper_bound = Q3 + 1.5 * IQR
+
+# Remove outliers
+data = data[(data['ApplicantIncome'] >= lower_bound) & (data['ApplicantIncome'] <= upper_bound)]
+
+
+
+
+# The income of the loan co-applicant also contains outliers. Let’s remove the outliers from this column as well:
+
+
+# Calculate the IQR
+
+Q1= data['CoapplicantIncome'].quantile(0.25)
+
+Q3 = data['CoapplicantIncome'].quantile(0.75)
+
+IQR = Q3 - Q1
+
+# Define the lower and upper bounds for outliers
+
+lower_bound = Q1 - 1.5 * IQR
+
+upper_bound = Q3 + 1.5 * IQR
+
+
+# Remove outliers
+
+data= data[(data['CoapplicantIncome'] >= lower_bound) & (data['CoapplicantIncome'] <= upper_bound)]
+
+
+
+plt.figure(figsize=(10, 6))
+sns.boxplot(data=data, x='Loan_Status', y='ApplicantIncome')
+plt.ylabel('Applicant Income')
+plt.xlabel('Education')
+plt.title('Box Plot: Applicant Income vs. Education')
+plt.savefig('Scatter Plot.png')
+plt.show()
+
+
+### The scatter plot after remove outliers from ApplicantIncome
+
+
+plt.figure(figsize=(10, 6))
+sns.scatterplot(data=data, x='ApplicantIncome', y='LoanAmount')
+plt.xlabel('Applicant Income')
+plt.ylabel('Loan Amount')
+plt.title('Scatter Plot: Applicant Income vs. Loan Amount')
+plt.savefig('scatterafter.png')
+plt.show()
+
+
+
+
+
+
+### Standardize the data - Feature Scaling
+
+
+
+
+# Scale the numerical columns using StandardScaler
+scaler = StandardScaler()
+numerical_cols = ['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term']
+data[numerical_cols] = scaler.fit_transform(data[numerical_cols])
+data[numerical_cols] = scaler.transform(data[numerical_cols])
+
+### Convert categorical Columns into numerical ones
+
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+col_to_encode = ['Gender','Dependents', 'Married', 'Education', 'Self_Employed', 'Property_Area','Loan_Status']
+for i in col_to_encode:
+    data[i] = le.fit_transform(data[i])
+
+data.head()
+data = data.drop(['Loan_ID'], axis = 1)
+# Split the dataset into features (X) and target (y)
+
+X = data.drop('Loan_Status', axis=1)
+y = data['Loan_Status']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,random_state=42)
+model = LogisticRegression() #define the model
+model.fit(X_train, y_train) #fit the model
+ypred = model.predict(X_test) #predict on test sample
+evaluation = f1_score(y_test, ypred)
+evaluation
+
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.svm import SVC
+
+import warnings
+warnings.filterwarnings("ignore")
+
+### Train the model
+log_model = LogisticRegression()
+log_model.fit(X_train, y_train)
+ypred_log = log_model.predict(X_test)
+
+
+print("Accuracy:", accuracy_score(y_test, ypred_log))
+print("Precision:", precision_score(y_test, ypred_log))
+print("Recall:", recall_score(y_test, ypred_log))
+print("F1 Score:", f1_score(y_test, ypred_log))
+joblib.dump(model, 'finall_loan_model.pkl')
